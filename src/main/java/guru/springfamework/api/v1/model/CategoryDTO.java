b51dcb800d8a12b69cc309956cd08cd78a36f707
Will Baymler <w.baymler@yandex.ru>
2018-12-19 17:17:41 +0300

    Add fixes to pom.xml. Create Bootstrap class and fix CategoryDTO.

--- src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
@@ -1,9 +1,21 @@
 package guru.springfamework.api.v1.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by jt on 9/24/17.
  */
+@Data
+@Entity
 public class CategoryDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
